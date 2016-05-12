c380ac303dfba5a86edbda685fcef841173be8b3
Adrián Fernández <adrian.ipod25@gmail.com>
2016-05-12 17:16:36 -0500

    login added

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php 
 include("./resources/header.php");
-session_start();
 if(!$_POST){
 	if(!(isset($_SESSION["isStarted"])) || $_SESSION["isStarted"] == FALSE){
 ?>
@@ -27,13 +26,13 @@ if(!$_POST){
 			</div>
 			<div class="col-md-12 text-center"> 
 				<h5>
-					<button type="submit" class="btn btn-success" style="margin:0;" ><a style="text-decoration:none;color:white;" href="./problems.php">
+					<button class="btn btn-success" style="margin:0;" ><a style="text-decoration:none;color:white;" href="./problems.php">
 
 						Ir a Problemas 
 						<span class="glyphicon glyphicon-education" aria-hidden="true">
 							
 						</span>
-														</a>
+						</a>
 					</button>
 				</h5>
 			</div>
@@ -61,6 +60,42 @@ if(!$_POST){
 					<button type="submit" class="btn btn-success">Iniciar Sesión</button>
 				</div>
 			</div>-->
+			<ul id="login-dp" class="dropdown-menu">
+				<li>
+					 <div class="row">
+							<div class="col-md-12">
+								Login via
+								<div class="social-buttons">
+									<a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+									<a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+								</div>
+                                or
+								 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+										<div class="form-group">
+											 <label class="sr-only" for="exampleInputEmail2">Email address</label>
+											 <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+										</div>
+										<div class="form-group">
+											 <label class="sr-only" for="exampleInputPassword2">Password</label>
+											 <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                             <div class="help-block text-right"><a href="">Forget the password ?</a></div>
+										</div>
+										<div class="form-group">
+											 <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+										</div>
+										<div class="checkbox">
+											 <label>
+											 <input type="checkbox"> keep me logged-in
+											 </label>
+										</div>
+								 </form>
+							</div>
+							<div class="bottom text-center">
+								New here ? <a href="#"><b>Join Us</b></a>
+							</div>
+					 </div>
+				</li>
+			</ul>
 		</form>
 
 	</div>
--- resources/header.php
+++ resources/header.php
@@ -1,23 +1,24 @@
 <?php 
-
+require_once("server.php");
 ?>
 <!DOCTYPE HTML>
 <html>
 <head>
-  <title>ESCOJ</title>
 	<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 	<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
 	<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  <script language="text/javascript" src="includes/scripts.js"></script>
 	<!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-	<script type="text/javascript" src="../js/jquery-2.2.0.min.js"></script>
-	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="../css/bootstrap.min.css">
-	<link rel="stylesheet" href="../css/bootstrap-theme.min.css">
-	<link rel="stylesheet" href="../css/main-style.css">
-	<link rel="stylesheet" href="../css/login.css">
-	<link rel="stylesheet" href="../css/style.css">
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<script type="text/javascript" src="./js/jquery-2.2.0.min.js"></script>
+	<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="./css/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="./css/bootstrap.min.css">
+	<link rel="stylesheet" href="./css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="./css/main-style.css">
+	<link rel="stylesheet" href="./css/login.css">
+	<link rel="stylesheet" href="./css/style.css">
 	<meta charset="utf-8">
 
 	
@@ -26,7 +27,7 @@
 <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
   <div class="container">
     <ul class="nav navbar-nav" >
-        <li><a href="./index.php"><img src="./media/escoj7.png" style=" width:175px; height:45px; position: relative; top:4px; left: 0px;"></a></li>
+        <li><img src="./media/escoj7.png" style=" width:175px; height:45px; position: relative; top:4px; left: 0px;"></li>
         <li style="margin:6px;"></li>
         <li class="active"><a href="#">Problemas <span class="sr-only">(current)</span></a></li>
         <li><a href="#">Estatus</a></li>
@@ -44,6 +45,48 @@
             <li><a href="#">One more separated link</a></li>
           </ul>
         </li>
+        <!--_________________________________Aqui_______________________________________-->
+        <li><p class="navbar-text">Already have an account?</p></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+      <ul id="login-dp" class="dropdown-menu">
+        <li>
+           <div class="row">
+              <div class="col-md-12">
+                Login via
+                <div class="social-buttons">
+                  <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
+                  <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
+                </div>
+                                or
+                 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                    <div class="form-group">
+                       <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                       <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                    </div>
+                    <div class="form-group">
+                       <label class="sr-only" for="exampleInputPassword2">Password</label>
+                       <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                             <div class="help-block text-right"><a href="">Forget the password ?</a></div>
+                    </div>
+                    <div class="form-group">
+                       <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                    </div>
+                    <div class="checkbox">
+                       <label>
+                       <input type="checkbox"> keep me logged-in
+                       </label>
+                    </div>
+                 </form>
+              </div>
+              <div class="bottom text-center">
+                New here ? <a href="#"><b>Join Us</b></a>
+              </div>
+           </div>
+        </li>
       </ul>
+        </li>
+      </ul>
+
   </div>
 </nav>
